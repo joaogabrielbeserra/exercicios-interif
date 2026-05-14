f22b9f8591b81d9433c7bd974d330fda22374ee8
cg3034593 <joao.beserra@aluno.ifsp.edu.br>
2026-05-13 21:27:43 -0300

    feat: att

--- src/boca_exercicios/carta.java
+++ src/boca_exercicios/carta.java
@@ -1,14 +1,14 @@
 
-
 import java.util.Scanner;
 
-public class Carta {
+public class carta {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
      String entrada = sc.next();
 
         int l = entrada.length();
+
         int nmatriz = 0;
         for(int i = 1; i <= 8 ; i++) {
             if(i*i == l){
@@ -17,16 +17,18 @@ public class Carta {
             }
         }
 
+        String palavra = "";
+
         if(nmatriz == 0) {
-            System.out.println("invalido");
+            System.out.print("invalido");
         } else {
-            for (int b = 0; b<=entrada.length(); b+=nmatriz) {
-                System.out.print(entrada.charAt(b));
+            for (int b = 0; b < (nmatriz*nmatriz); b+=(nmatriz)) {
+                palavra += entrada.charAt(b);
                 b++;
             }
         }
 
-
+        System.out.println(palavra);
 
 
         }
